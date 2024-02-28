f20dd03146166624511ce71eab12557d14052271
eastriver <eastriver0720@gmail.com>
2024-02-28 17:40:54 +0900

    add more quantizations

--- Sources/LLM/LLM.swift
+++ Sources/LLM/LLM.swift
@@ -565,15 +565,28 @@ public struct Template {
 }
 
 public enum Quantization: String {
+    case IQ1_S
     case IQ2_XXS
     case IQ2_XS
+    case IQ2_S
+    case IQ2_M
     case Q2_K_S
     case Q2_K
+    case IQ3_XXS
+    case IQ3_XS
+    case IQ3_S
+    case IQ3_M
     case Q3_K_S
     case Q3_K_M
     case Q3_K_L
+    case IQ4_XS
+    case IQ4_NL
+    case Q4_0
+    case Q4_1
     case Q4_K_S
     case Q4_K_M
+    case Q5_0
+    case Q5_1
     case Q5_K_S
     case Q5_K_M
     case Q6_K
